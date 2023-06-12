8c54f09b2e9f03683279ce4d79be2c2e000a7d89
Andrew Nascimento <81823870+AndrewPolengolas@users.noreply.github.com>
2023-06-12 11:48:49 -0300

    criando projeto

---
+++ src/main/java/com/eatrack/EaTrackApplication.java
@@ -0,0 +1,13 @@
+package com.eatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatrack/EaTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
